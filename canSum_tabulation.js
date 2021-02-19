e9f8ab3b5cd993f382c6a327fd30bd3f703e500f
Michael Scoggins <rhythmghosts@gmail.com>
2021-02-18 23:33:16 -0600

    cleaner implementation of canSum_tabulation

--- canSum_tabulation.js
+++ canSum_tabulation.js
@@ -1,12 +1,10 @@
 const canSum = (targetSum, numbers) => {
   const table = Array(targetSum + 1).fill(false);
   table[0] = true;
-  for (let i = 0; i <= table.length; i++) {
+  for (let i = 0; i <= targetSum; i++) {
     if (table[i] === true) {
       for (let num of numbers) {
-        if (table[i + num] < table.length) {
-          table[i + num] = true;
-        }
+        table[i + num] = true;
       }
     }
   }
